e2660b337d368ca9fea3b1b90aad5f52dedd7fda
Diego Aguirre <diego.a.aguirre.alvarez@gmail.com>
2022-12-07 15:22:15 -0400

    creacion layout y principio de index

--- reusaMain/reusaMain/urls.py
+++ reusaMain/reusaMain/urls.py
@@ -15,7 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from reusaApp import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('inicio', views.index, name='index'),
+    path('', views.index, name="Inicio")
 ]
